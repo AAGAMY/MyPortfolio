3295907b89af8a786d2efe3fcee0420a8080870e
AG <AG@DEVELOPMENT>
2022-12-18 01:52:58 +0200

    Add project files.

---
+++ Core/Entities/EntityBase.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Core.Entities
+{
+    public class EntityBase
+    {
+        public Guid ID { get; set; }
+    }
+}
---
+++ Core/Entities/Owner.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Entities
+{
+    public class Owner : EntityBase
+    {
+        public string FullName { get; set; }
+        public string Name { get; set; }
+        public string Avatar { get; set; }
+        public Address address { get; set; }
+
+    }
+
+}
